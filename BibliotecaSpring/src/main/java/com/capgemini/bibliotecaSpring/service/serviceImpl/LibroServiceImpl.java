992eac5a4b7ed0d64753fbc2465d181dcc883995
Kilopolo <34222746+Kilopolo@users.noreply.github.com>
2023-10-25 15:24:52 +0200

    Interfaz de interfaces services

---
+++ BibliotecaSpring/src/main/java/com/capgemini/bibliotecaSpring/service/serviceImpl/LibroServiceImpl.java
@@ -0,0 +1,5 @@
+package com.capgemini.bibliotecaSpring.service.serviceImpl;
+
+public class LibroServiceImpl {
+
+}
--- BibliotecaSpring/src/main/java/com/capgemini/bibliotecaSpring/serviceImpl/LibroServiceImpl.java
+++
@@ -1,5 +0,0 @@
-package com.capgemini.bibliotecaSpring.serviceImpl;
-
-public class LibroServiceImpl {
-
-}
