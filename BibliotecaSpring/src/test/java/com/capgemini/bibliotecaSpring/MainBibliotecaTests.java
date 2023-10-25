eff0eec46ac4ce690de607760041dd3a90d6d7f5
Kilopolo <34222746+Kilopolo@users.noreply.github.com>
2023-10-25 13:22:30 +0200

    Funciona localhost

--- BibliotecaSpring/src/test/java/com/capgemini/bibliotecaSpring/MainBibliotecaTests.java
+++ BibliotecaSpring/src/test/java/com/capgemini/bibliotecaSpring/MainBibliotecaTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Prueba2ApplicationTests {
+class MainBibliotecaTests {
 
 	@Test
 	void contextLoads() {
