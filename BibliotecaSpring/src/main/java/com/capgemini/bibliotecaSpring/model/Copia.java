e8042285fa5967c6b96bb52c8d6cbd89497d19e7
Patricia <118187535+patrii993@users.noreply.github.com>
2023-10-25 13:40:13 +0200

    Arreglo proyecto

--- BibliotecaSpring/src/main/java/com/capgemini/bibliotecaSpring/model/Copia.java
+++ BibliotecaSpring/src/main/java/com/capgemini/bibliotecaSpring/model/Copia.java
@@ -1,6 +1,5 @@
 package com.capgemini.bibliotecaSpring.model;
 
-import enumerado.EstadoCopia;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -22,5 +21,5 @@ public class Copia {
 	@Column
 	private Long idcopia;
 	@Column
-	private EstadoCopia estado;
+	private String estado;
 }
