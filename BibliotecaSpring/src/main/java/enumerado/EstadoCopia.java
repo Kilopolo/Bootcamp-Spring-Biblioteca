129288ccc6bc880c49961533f058c9a4e5a24ffe
Patricia <118187535+patrii993@users.noreply.github.com>
2023-10-25 14:00:53 +0200

    quitar paquete y enumerados

--- BibliotecaSpring/src/main/java/enumerado/EstadoCopia.java
+++
@@ -1,5 +0,0 @@
-package enumerado;
-
-public enum EstadoCopia {
-	PRESTADO, RETRASO, BIBLIOTECA,REPARACION
-}
--- BibliotecaSpring/src/main/java/enumerado/TipoLibro.java
+++
@@ -1,5 +0,0 @@
-package enumerado;
-
-public enum TipoLibro {
-	NOVELA,TEATRO,POESIA,ENSAYO
-}
