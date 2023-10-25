8974d4ba2ee4fe817c00f12029dd69cf2db410f6
Kilopolo <34222746+Kilopolo@users.noreply.github.com>
2023-10-25 17:44:06 +0200

    a

--- BibliotecaSpring/src/main/java/com/capgemini/bibliotecaSpring/controller/Controlador.java
+++ BibliotecaSpring/src/main/java/com/capgemini/bibliotecaSpring/controller/Controlador.java
@@ -2,7 +2,7 @@ package com.capgemini.bibliotecaSpring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.capgemini.bibliotecaSpring.service.LibroService;
 
@@ -12,7 +12,7 @@ public class Controlador {
 	@Autowired
 	private LibroService service;
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String home() {
 		System.out.println("home??");
 		return "index";
