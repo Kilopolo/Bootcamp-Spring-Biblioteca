1bac7cab98c774a35463569f2354b782c3b886e3
Kilopolo <34222746+Kilopolo@users.noreply.github.com>
2023-10-25 16:48:19 +0200

    services

--- BibliotecaSpring/src/main/java/com/capgemini/bibliotecaSpring/service/LectorService.java
+++ BibliotecaSpring/src/main/java/com/capgemini/bibliotecaSpring/service/LectorService.java
@@ -6,6 +6,19 @@ import com.capgemini.bibliotecaSpring.model.Lector;
 
 public interface LectorService extends ServiceS<Lector> {
 
-	void devolver(long id, LocalDate date);
+	/**
+	 * {precondicion: prestamos.notEmpty()}
+	 * @param id
+	 * @param date
+	 */
+	public void devolver(long id, LocalDate fechaAct);
+	/**
+	 * {precondicion: multa == 0}
+	 * @param id
+	 * @param fechaAct
+	 */
+	public void prestar(long id, LocalDate fechaAct);
+	
+	void multar(int dias);
 	
 }
--- BibliotecaSpring/src/main/java/com/capgemini/bibliotecaSpring/service/serviceImpl/LectorServiceImpl.java
+++ BibliotecaSpring/src/main/java/com/capgemini/bibliotecaSpring/service/serviceImpl/LectorServiceImpl.java
@@ -7,17 +7,27 @@ import org.springframework.stereotype.Service;
 import com.capgemini.bibliotecaSpring.model.Lector;
 import com.capgemini.bibliotecaSpring.service.LectorService;
 
-
 @Service
-public class LectorServiceImpl extends ServiceImpl<LectorService, Lector> implements LectorService{
+public class LectorServiceImpl extends ServiceImpl<LectorService, Lector> implements LectorService {
+
+	private final int MAX_LIBROS = 3;
 
 	@Override
 	public void devolver(long id, LocalDate date) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	@Override
+	public void prestar(long id, LocalDate fechaAct) {
+		// TODO Auto-generated method stub
 
+	}
 
+	@Override
+	public void multar(int dias) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
