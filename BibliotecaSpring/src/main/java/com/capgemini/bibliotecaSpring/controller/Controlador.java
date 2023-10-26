5d56ad8d4ba0f10c46fcf478991276bb90c47f8d
Patricia <118187535+patrii993@users.noreply.github.com>
2023-10-26 16:58:12 +0200

    add mostrar autores

--- BibliotecaSpring/src/main/java/com/capgemini/bibliotecaSpring/controller/Controlador.java
+++ BibliotecaSpring/src/main/java/com/capgemini/bibliotecaSpring/controller/Controlador.java
@@ -1,17 +1,13 @@
 package com.capgemini.bibliotecaSpring.controller;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.capgemini.bibliotecaSpring.model.Autor;
 import com.capgemini.bibliotecaSpring.model.Lector;
-
 import com.capgemini.bibliotecaSpring.model.Libro;
 import com.capgemini.bibliotecaSpring.service.AutorService;
 import com.capgemini.bibliotecaSpring.service.LibroService;
@@ -29,27 +25,39 @@ public class Controlador {
 	        System.out.println("Hola, mundo");
 	        return "index";
 	    }
+	    
+	    @GetMapping("/autores")
+	    public String mostrarAutores(Model modelo) {
+	        modelo.addAttribute("autores", autorservice.getAll());
+	        return "autor/mostrar";
+	    }
 		@GetMapping("/addlector")
-		public String showNewLectorForm(Model modelo) {
+		public String formlector(Model modelo) {
 			Lector lector = new Lector();
 			modelo.addAttribute("lector", lector);
 			return "lector/addLector";
 		}
 		@GetMapping("/addautor")
-		public String showNewAutorForm(Model modelo) {
+		public String formautor(Model modelo) {
 			Autor autor = new Autor();
 			modelo.addAttribute("autor", autor);
 			return "autor/addAutor";
 		}
 	    
 	    @GetMapping("/addlibro")
-		public String addLibro(Model model) {
+		public String formlibro(Model model) {
 			Libro libro=new Libro();
 			model.addAttribute("autores",autorservice.getAll());
 			model.addAttribute("libro", libro);
 			return "libro/addLibro";
 		}
-	
+		@GetMapping("/delete/1")
+		public String eliminar( @RequestParam("idautor") long idautor, Model modelo) {
+			
+				autorservice.deleteById(idautor);
+				return "redirect:/";
+			
+		}
 	
 
 }
