61adb667e56784a8344ecd7afa69060c0cb12fac
Kilopolo <34222746+Kilopolo@users.noreply.github.com>
2023-10-26 13:15:59 +0200

    security empezado

--- BibliotecaSpring/src/main/java/com/capgemini/bibliotecaSpring/service/LibroService.java
+++ BibliotecaSpring/src/main/java/com/capgemini/bibliotecaSpring/service/LibroService.java
@@ -1,7 +1,10 @@
 package com.capgemini.bibliotecaSpring.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.capgemini.bibliotecaSpring.model.Libro;
 
 public interface LibroService extends ServiceS<Libro> {
-	
+	public Page<Libro> searchOffersByTitle(Pageable p, String searchText);
 	}
