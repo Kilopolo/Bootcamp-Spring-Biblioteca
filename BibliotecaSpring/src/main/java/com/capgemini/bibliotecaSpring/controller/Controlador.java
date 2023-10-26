392c838eaa5ce504677002af74b11fa2031dd5bc
DavidSantosM <114166018+DavidSantosM@users.noreply.github.com>
2023-10-26 13:19:36 +0200

    addLibros.html

--- BibliotecaSpring/src/main/java/com/capgemini/bibliotecaSpring/controller/Controlador.java
+++ BibliotecaSpring/src/main/java/com/capgemini/bibliotecaSpring/controller/Controlador.java
@@ -2,10 +2,12 @@ package com.capgemini.bibliotecaSpring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.capgemini.bibliotecaSpring.model.Libro;
 import com.capgemini.bibliotecaSpring.service.LibroService;
 
 @Controller
@@ -17,8 +19,15 @@ public class Controlador {
 	    @GetMapping("/")
 	    public String helloWorld() {
 	        System.out.println("Hola, mundo");
-	        return "index.html";
+	        return "index";
 	    }
+	    
+	    @GetMapping("/addlibro")
+		public String addLibro(Model model) {
+			Libro libro=new Libro();
+			model.addAttribute("libro", libro);
+			return "libro/addLibro";
+		}
 	
 	
 
