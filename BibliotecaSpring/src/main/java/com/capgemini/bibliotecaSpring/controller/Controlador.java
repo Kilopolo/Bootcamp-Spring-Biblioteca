fd3c12c94aba19ebb1a8ccc2b63381167ea1a7c8
Patricia <118187535+patrii993@users.noreply.github.com>
2023-10-26 13:19:44 +0200

    addautor y addlector retoque fragment

--- BibliotecaSpring/src/main/java/com/capgemini/bibliotecaSpring/controller/Controlador.java
+++ BibliotecaSpring/src/main/java/com/capgemini/bibliotecaSpring/controller/Controlador.java
@@ -2,10 +2,11 @@ package com.capgemini.bibliotecaSpring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.capgemini.bibliotecaSpring.model.Autor;
+import com.capgemini.bibliotecaSpring.model.Lector;
 import com.capgemini.bibliotecaSpring.service.LibroService;
 
 @Controller
@@ -19,7 +20,18 @@ public class Controlador {
 	        System.out.println("Hola, mundo");
 	        return "index.html";
 	    }
-	
+		@GetMapping("/addlector")
+		public String showNewLectorForm(Model modelo) {
+			Lector lector = new Lector();
+			modelo.addAttribute("lector", lector);
+			return "lector/addLector";
+		}
+		@GetMapping("/addautor")
+		public String showNewAutorForm(Model modelo) {
+			Autor autor = new Autor();
+			modelo.addAttribute("autor", autor);
+			return "autor/addAutor";
+		}
 	
 
 }
