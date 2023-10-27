9da5145ef46354c49d096aea7a1fa3baca711af8
Kilopolo <34222746+Kilopolo@users.noreply.github.com>
2023-10-27 08:50:12 +0200

    .

--- BibliotecaSpring/src/main/java/com/capgemini/bibliotecaSpring/service/serviceImpl/LibroServiceImpl.java
+++ BibliotecaSpring/src/main/java/com/capgemini/bibliotecaSpring/service/serviceImpl/LibroServiceImpl.java
@@ -1,11 +1,5 @@
 package com.capgemini.bibliotecaSpring.service.serviceImpl;
 
-import java.util.LinkedList;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.capgemini.bibliotecaSpring.model.Libro;
