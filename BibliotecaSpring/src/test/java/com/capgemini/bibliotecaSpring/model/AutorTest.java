6016dfdf059fe4e9c00a305e7734740a641027bb
Kilopolo <34222746+Kilopolo@users.noreply.github.com>
2023-10-30 15:56:11 +0100

    arreglos html

--- BibliotecaSpring/src/test/java/com/capgemini/bibliotecaSpring/model/AutorTest.java
+++ BibliotecaSpring/src/test/java/com/capgemini/bibliotecaSpring/model/AutorTest.java
@@ -1,36 +1,53 @@
 package com.capgemini.bibliotecaSpring.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.capgemini.bibliotecaSpring.service.AutorService;
+import com.capgemini.bibliotecaSpring.service.serviceInterfaces.AutorService;
+ 
 @SpringBootTest
 class AutorTest {
+ 
 	@Autowired
 	AutorService autorservice;
-
+ 
 	/*
 	 * @BeforeAll static void setUpBeforeClass() throws Exception { }
-	 * 
+	 *
 	 * @AfterAll static void tearDownAfterClass() throws Exception { }
-	 * 
+	 *
 	 * @BeforeEach void setUp() throws Exception { }
-	 * 
+	 *
 	 * @AfterEach void tearDown() throws Exception { }
 	 */
-
+ 
 	@Test
-	void test1() {
-		System.out.println(autorservice.getById(1L).getNombre());
+	void testFindById() {
+		System.out.println("Test 1: " + autorservice.getById(1L).getNombre());
 		Autor autor = autorservice.getById(1L);
-		assertEquals("Bram Stoker",autor.getNombre());
+		assertEquals("Bram Stoker", autor.getNombre());
 	}
-
+ 
+	@Test
+	void testSave() {
+		Autor autor = new Autor(10L, "Cervantes", "Española", LocalDate.of(1547, 9, 29), new ArrayList<Libro>());
+		autorservice.save(autor);
+		assertEquals("Cervantes", autor.getNombre());
+		System.out.println("Test 2: " + autorservice.getById(10L).getNombre());
+	}
+ 
+	@Test
+	void testDeleteById() {
+		Autor autor = autorservice.getById(10L);
+		autorservice.deleteById(autor.getIdautor());
+		assertEquals(10L, autor.getIdautor());
+		System.out.println("Test 3: " + autor.getNombre());
+	}
+ 
 }
