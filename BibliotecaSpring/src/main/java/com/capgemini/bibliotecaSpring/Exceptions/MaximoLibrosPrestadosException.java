295e2af202277c15ac61627a00f32cf6d78fb4ee
Kilopolo <34222746+Kilopolo@users.noreply.github.com>
2023-11-02 11:43:55 +0100

    Funciona mergeos

--- BibliotecaSpring/src/main/java/com/capgemini/bibliotecaSpring/Exceptions/MaximoLibrosPrestadosException.java
+++ BibliotecaSpring/src/main/java/com/capgemini/bibliotecaSpring/Exceptions/MaximoLibrosPrestadosException.java
@@ -1,4 +1,4 @@
-package com.capgemini.bibliotecaSpring.Exceptions;
+package com.capgemini.bibliotecaSpring.exceptions;
 
 public class MaximoLibrosPrestadosException extends RuntimeException {
 	/**
