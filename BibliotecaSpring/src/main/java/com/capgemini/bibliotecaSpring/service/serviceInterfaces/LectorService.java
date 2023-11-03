50709ded718993d668fa231093766086583e38e4
Patricia <118187535+patrii993@users.noreply.github.com>
2023-11-03 12:44:16 +0100

    cambios

--- BibliotecaSpring/src/main/java/com/capgemini/bibliotecaSpring/service/serviceInterfaces/LectorService.java
+++ BibliotecaSpring/src/main/java/com/capgemini/bibliotecaSpring/service/serviceInterfaces/LectorService.java
@@ -1,19 +1,8 @@
 package com.capgemini.bibliotecaSpring.service.serviceInterfaces;
 
 import com.capgemini.bibliotecaSpring.model.Lector;
-import com.capgemini.bibliotecaSpring.model.Prestamo;
 import com.capgemini.bibliotecaSpring.service.ServiceS;
 
 public interface LectorService extends ServiceS<Lector> {
 
-
-	/**
-	 * {precondicion: multa == 0}
-	 * 
-	 * @param id
-	 * @param fechaAct
-	 */	
-
-
-	void multar(long idLector, Prestamo prestamo);
 }
